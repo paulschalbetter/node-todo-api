e585d944a91f81d8978c161f6fd981c33bafcd0d
root <root@srvtest01.pascha.local>
2019-01-13 16:07:55 +0100

    starting builind the api

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+
+const port = process.env.PORT || 3000;
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  let todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos', (req, res) => {
+  console.log('lol');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
