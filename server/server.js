67b51b49a7002baf5672d70e09b4f9e240965617
root <root@srvtest01.pascha.local>
2019-01-13 16:55:34 +0100

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
-
 const port = process.env.PORT || 3000;
 let app = express();
 
