b9b913a64e29cb812d64eda92ca326c72c2b44b3
root <root@srvtest01.pascha.local>
2019-01-13 16:45:32 +0100

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -23,7 +23,11 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-  console.log('lol');
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(port, () => {
