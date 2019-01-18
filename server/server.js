39ee9fb4d52d8b1ebc829e74d6abd8eaa2af634e
root <root@srvtest01.pascha.local>
2019-01-18 21:34:40 +0100

    Add POST /users/login method and tests

--- server/server.js
+++ server/server.js
@@ -116,7 +116,9 @@ app.post('/users/login', (req, res) => {
   let body = _.pick(req.body, ['email', 'password']);
 
   User.findByCredentials(body.email, body.password).then((user) => {
-    res.send(user);
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
   }).catch((e) => {
     res.status(400).send();
   });
